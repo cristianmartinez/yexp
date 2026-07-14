4dd3b25219221682c501895e1cc8f6eaf2fb45c9
Cristian Martinez <dev.cmartinez@gmail.com>
2026-07-14 23:48:47 +0200

    docs: publish language specification

--- packages/playground/content/_meta.js
+++ packages/playground/content/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: 'Introduction',
   'getting-started': 'Getting Started',
   syntax: 'Language Guide',
+  spec: 'Language Specification',
   architecture: 'Architecture',
   'vm-execution': 'VM Execution Visualizer',
 };
